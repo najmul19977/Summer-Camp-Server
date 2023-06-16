157d6fbe81ab66fa620c7ca11b5e5fab25b949bf
najmul19977 <najmulkabir19977@gmail.com>
2023-06-16 21:36:56 +0600

    jwt commit

--- index.js
+++ index.js
@@ -10,6 +10,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req,res,next) =>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error:true,message:'unauthorized access'});
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) =>{
+    if(err){
+      return res.status(401).send({error:true,message:'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-56gcpdx-shard-00-00.mgbkpcq.mongodb.net:27017,ac-56gcpdx-shard-00-01.mgbkpcq.mongodb.net:27017,ac-56gcpdx-shard-00-02.mgbkpcq.mongodb.net:27017/?ssl=true&replicaSet=atlas-neronq-shard-0&authSource=admin&retryWrites=true&w=majority`;
@@ -93,10 +108,15 @@ async function run() {
     });
 
     // cart collection apis
-    app.get('/carts',async(req,res) =>{
+    app.get('/carts',verifyJWT, async(req,res) =>{
       const email =req.query.email;
       if(!email){
         res.send([]);
+      }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail){
+        return res.status(403).send({error:true,message:'porviden access'})
       }
       const query = {email:email};
       const result = await cartCollection.find(query).toArray();

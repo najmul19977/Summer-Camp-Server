5e4006fb262e0bfda35527f5ac4da14306204c61
najmul19977 <najmulkabir19977@gmail.com>
2023-06-17 23:57:14 +0600

    anathe commit

--- index.js
+++ index.js
@@ -10,15 +10,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const verifyJWT = (req,res,next) =>{
-  const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error:true,message:'unauthorized access'});
+const verifyJWT = (req, res, next) => {
+  const authorization = req?.headers?.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
   }
   const token = authorization.split(' ')[1];
-  jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,decoded) =>{
-    if(err){
-      return res.status(401).send({error:true,message:'unauthorized access'})
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
     }
     req.decoded = decoded;
     next();
@@ -50,122 +50,145 @@ async function run() {
     const cartCollection = client.db("summerCampDb").collection("carts");
     const usersCollection = client.db("summerCampDb").collection("users");
 
-    app.post('/jwt',(req,res) =>{
+    app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET,{ expiresIn:'1h'})
-      res.send({token})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
+      res.send({ token })
     });
 
-    const verifyAdmin = async(req,res,next) =>{
+    const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
-      const query = {email:email}
+      const query = { email: email }
       const user = await usersCollection.findOne(query);
-      if(user?.role !== 'admin'){
-        return res.status(403).send({error:true,message:'forbidden message'});
+      if (user?.role !== 'admin') {
+        return res.status(403).send({ error: true, message: 'forbidden message' });
       }
       next();
     }
-  // user relatet api
-    app.get('/users',async(req,res) =>{
+    // user relatet api
+    app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
     });
 
-    
-    app.post('/users',async(req,res) =>{
+
+    app.post('/users', async (req, res) => {
       const user = req.body;
-     
-      const query = {email:user.email}
-      const existingUser = await usersCollection.findOne(user);
-    
-      if(existingUser){
-        return res.send({message:'user already exists'})
+
+      const query = { email: user?.email }
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: 'user already exists' })
       }
       const result = await usersCollection.insertOne(user);
       res.send(result);
     });
 
-    app.get('/users/admin/:email',verifyJWT, verifyAdmin,async(req,res) =>{
+    app.get('/users/admin/:email',  async (req, res) => {
       const email = req.params.email;
-       
-      if(req.decoded.email !== email){
-        res.send({admin:false})
+
+      if (req.decoded.email !== email) {
+        res.send({ admin: false })
       }
 
-      const query = {email:email}
+      const query = { email: email }
       const user = await usersCollection.findOne(query);
-      const result = {admin:user?.role === 'admin'}
+      const result = { admin: user?.role === 'admin' }
       res.send(result);
     })
-    
-    app.patch('/users/admin/:id',async(req,res) =>{
+
+    app.patch('/users/admin/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updateDoc = {
-        $set:{
-          role:'admin'
+        $set: {
+          role: 'admin'
         },
       };
-      const result = await usersCollection.updateOne(filter,updateDoc);
+      const result = await usersCollection.updateOne(filter, updateDoc);
       res.send(result);
     })
 
 
- //get data
-    app.get('/allclass',async(req,res) =>{
-        const result = await allClassCollection.find().toArray();
-        res.send(result);
+    //get data
+    app.get('/allclass', async (req, res) => {
+      const result = await allClassCollection.find().toArray();
+      res.send(result);
     });
-  // add new class 
-    app.post('/allclass',verifyJWT,verifyAdmin, async(req,res) =>{
+    // add new class 
+    app.post('/allclass',  verifyAdmin, async (req, res) => {
       const newItem = req.body;
       const result = await allClassCollection.insertOne(newItem);
       res.send(result);
     });
-    app.delete ('/allclass/:id',verifyJWT,verifyAdmin,async(req,res) =>{
+    app.delete('/allclass/:id',  verifyAdmin, async (req, res) => {
       const id = req.params.id;
-      const query = {_id:new ObjectId(id)}
+      const query = { _id: new ObjectId(id) }
       const result = await allClassCollection.deleteOne(query);
       res.send(result);
     })
 
-    app.get('/popularclass',async(req,res) =>{
-        const result = await popularClassCollection.find().toArray();
-        res.send(result);
+    app.get('/popularclass', async (req, res) => {
+      const result = await popularClassCollection.find().toArray();
+      res.send(result);
     });
-    app.get('/allInstractor',async(req,res) =>{
-        const result = await allInstractorCollection.find().toArray();
-        res.send(result);
+    app.get('/allInstractor', async (req, res) => {
+      const result = await allInstractorCollection.find().toArray();
+      res.send(result);
     });
-    app.get('/popularInstractor',async(req,res) =>{
-        const result = await popularInstractorCollection.find().toArray();
-        res.send(result);
+    app.get('/popularInstractor', async (req, res) => {
+      const result = await popularInstractorCollection.find().toArray();
+      res.send(result);
     });
 
+    //admin collcetion data
+    app.get('/admin-status', verifyAdmin, async (req, res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const allclass = await allClassCollection.estimatedDocumentCount();
+      const popularclass = await popularClassCollection.estimatedDocumentCount();
+      const addcart = await cartCollection.estimatedDocumentCount();
+      const allinstractur = await allInstractorCollection.estimatedDocumentCount();
+      const populerinstractur = await popularInstractorCollection.estimatedDocumentCount();
+
+      res.send({
+        users,
+        allclass,
+        popularclass,
+        addcart,
+        allinstractur,
+        populerinstractur
+
+
+
+      });
+
+    })
+
     // cart collection apis
-    app.get('/carts',verifyJWT, async(req,res) =>{
-      const email =req.query.email;
-      if(!email){
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email;
+      if (!email) {
         res.send([]);
       }
 
       const decodedEmail = req.decoded.email;
-      if(email !== decodedEmail){
-        return res.status(403).send({error:true,message:'porviden access'})
+      if (email !== decodedEmail) {
+        return res.status(403).send({ error: true, message: 'porviden access' })
       }
-      const query = {email:email};
+      const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
-    app.post('/carts',async(req,res) =>{
+    app.post('/carts', async (req, res) => {
       const item = req.body;
       console.log(item);
       const result = await cartCollection.insertOne(item);
       res.send(result);
     });
-    app.delete('/carts/:id',async(req,res) =>{
+    app.delete('/carts/:id', async (req, res) => {
       const id = req.params.id
-      const query = {_id:new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
@@ -179,12 +202,12 @@ async function run() {
   }
 }
 run().catch(console.dir);
- 
 
-app.get('/',(req,res) =>{
-    res.send('summer camp is running')
+
+app.get('/', (req, res) => {
+  res.send('summer camp is running')
 })
 
-app.listen(port,() =>{
-    console.log(`summer camp is running on port ${port}`);
+app.listen(port, () => {
+  console.log(`summer camp is running on port ${port}`);
 })
